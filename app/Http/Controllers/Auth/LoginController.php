acd7f3eca574de923310be2353b772928c83eea1
micsedinam <michten35@gmail.com>
2022-08-14 22:05:22 +0000

    Updates to course registration

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,10 +50,15 @@ class LoginController extends Controller
         if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
         {
             if (auth()->user()->is_admin == 1) {
+
                 return redirect()->route('admin.home');
+
             }elseif (auth()->user()->is_admin == 2) {
+
                 return redirect()->route('student.home');
+
             }else{
+                
                 return redirect()->route('user.home');
             }
         }else{
